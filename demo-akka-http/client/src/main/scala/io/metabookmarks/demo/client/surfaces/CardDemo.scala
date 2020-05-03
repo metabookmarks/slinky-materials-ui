5d1c9513bcef1b7e70f5679a431e8ccd3849fdb3
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2020-05-03 22:45:03 +0200

    WIP Card

--- demo-akka-http/client/src/main/scala/io/metabookmarks/demo/client/surfaces/CardDemo.scala
+++ demo-akka-http/client/src/main/scala/io/metabookmarks/demo/client/surfaces/CardDemo.scala
@@ -7,24 +7,34 @@ import slinky.core.facade.ReactElement
 
 import slinky.web.html._
 import slinky.materialui.core._
+import scalajs.js
+import scala.scalajs.js.annotation.JSImport
+
+object ReactTestEvent {
+  @js.native
+  @JSImport("@material-ui/core", JSImport.Default)
+  object Module extends js.Any {
+    @js.native
+    object makeStyles extends js.Object
+  }
+  val makeStyles = Module.makeStyles.asInstanceOf[js.Function1[js.Object, js.Object]]
+}
+
 @react class CardDemo extends StatelessComponent {
   type Props = Unit
 
-  def render(): ReactElement =
-    Grid(container = true, wrap = Wrap.wrap)(
-      (1 to 3).map(i =>
+  def render(): ReactElement = {
+    println(ReactTestEvent.Module.makeStyles)
+    Grid(container = true, wrap = Wrap.wrap, spacing = 2)(
+      (1 to 12).map(i =>
         Grid(key = s"k$i", item = true, sm = Size.`2`)(
-          Card(
-            CardActionArea(
-              CardMedia(
-                src = "/assets/img/burgers.jpg",
-                title = "Click me"
-              )
-            ),
-            CardContent(span("Coucou"))
+          Card(cla
+            CardContent(
+              Typography("iiz zizi zi ziz i ziziz izi ")
+            )
           )
         )
       )
     )
-
+  }
 }
