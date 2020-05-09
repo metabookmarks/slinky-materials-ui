73b652e80d65dcaedd00ea92c9b134caa0d21565
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2020-05-09 19:10:47 +0200

    Minor updates

--- demo-akka-http/client/src/main/scala/io/metabookmarks/demo/client/surfaces/ProfileDemo.scala
+++ demo-akka-http/client/src/main/scala/io/metabookmarks/demo/client/surfaces/ProfileDemo.scala
@@ -12,40 +12,84 @@ import io.metabookmarks.demo.shared.User
 import slinky.core.FunctionalComponent
 
 import js.Dynamic.{literal => CSS}
+import io.metabookmarks.demo.shared.Profile
 
 @js.native
 trait Styles extends js.Object {
   val root: String = js.native
+  val paper: String = js.native
+  val image: String = js.native
+  val img: String = js.native
 }
 
 @react object ProfileDemo {
   case class Props(user: User)
   val useStyles = makeStyles[Styles](
     "root" -> CSS(
-      "flexGrow" -> 1,
-      "toto" -> 2
+      "flexGrow" -> 1
     ),
-    "test" -> CSS("tata" -> "ooo")
+    "paper" -> CSS(
+      "margin" -> "auto",
+      "maxWidth" -> 500
+    ),
+    "image" -> CSS(
+      "width" -> 128,
+      "heigh" -> 128
+    ),
+    "img" -> CSS(
+      "margin" -> "auto",
+      "display" -> "block",
+      "maxWidth" -> "100%",
+      "maxHeight" -> "100%"
+    )
   )
 
   val component = FunctionalComponent[Props] { props =>
-    val styles: Styles = useStyles()
+    val classes: Styles = useStyles()
 
-    div(className := styles.root)(
-      GridContainer(wrap = Wrap.wrap, alignItems = Align.stretch, alignContent = Align.stretch)(
+    def userPanel(user: User) = Paper(className = classes.paper)(
+      GridContainer(spacing = 2)(
         GridItem(key = s"photo")(
-          Paper(img(src := props.user.avatarURL))
+          ButtonBase(className = classes.image)(
+            img(className := classes.img, src := user.avatarURL)
+          )
         ),
-        GridItem(key = s"gen")(
-          Paper(
-            CardContent(
-              Typography(props.user.email),
-              Typography(props.user.firstName),
-              Typography(props.user.lastName)
+        Grid(key = s"gen", sm = true, xs = Size.`12`)(
+          Grid(xs = true, direction = "column", spacing = 2)(
+            GridItem(xs = true)(
+              Typography(gutterBottom = true, variant = "subtitle1")(user.email),
+              Typography(user.firstName),
+              Typography(user.lastName)
             )
           )
         )
       )
+    )
+
+    def profilePanel(profile: Profile) = Paper(className = classes.paper)(
+      GridContainer(spacing = 2)(
+        GridItem(key = s"photo")(
+          ButtonBase(className = classes.image)(
+            img(className := classes.img, src := profile.avatarURL)
+          )
+        ),
+        Grid(key = s"gen", sm = true, xs = Size.`12`)(
+          Grid(xs = true, direction = "column", spacing = 2)(
+            GridItem(xs = true)(
+              Typography(gutterBottom = true, variant = "subtitle1")(profile.providerKey),
+              Typography(profile.fullName)
+            )
+          )
+        )
+      )
+    )
+
+    div(className := classes.root)(
+      userPanel(props.user),
+      props.user.profiles.map {
+        case (provider, profile) =>
+          profilePanel(profile)
+      }
     )
   }
 }
