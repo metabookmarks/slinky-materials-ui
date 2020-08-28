5f42a91355a430e4e5091bd3fe812320584b8052
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2020-08-28 19:24:54 +0200

    :sparkles: Add ButtonGroup

--- dev/src/main/scala/io/metabookmarks/demo/client/mdc/ButtonsDemo.scala
+++ dev/src/main/scala/io/metabookmarks/demo/client/mdc/ButtonsDemo.scala
@@ -4,25 +4,17 @@ import slinky.core.annotations.react
 import slinky.core.StatelessComponent
 import slinky.core.facade.ReactElement
 import slinky.web.html._
+import slinky.materialui.core._
 
 @react class ButtonsDemo extends StatelessComponent {
 
   type Props = Unit
 
   def render(): ReactElement =
-    div(
-      div(className := "mdc-touch-target-wrapper")(
-        button(className := "mdc-button mdc-button--touch")(
-          div(className := "mdc-button__ripple"),
-          span(className := "mdc-button__label")("My Accessible Button"),
-          div(className := "mdc-button__touch")
-        )
-      ),
-      button(className := "mdc-button")(
-        div(className := "mdc-button__ripple"),
-        i(className := "material-icons mdc-button__icon", aria - "hidden" := "true")("bookmark"),
-        span(className := "mdc-button__label")("Text Button plus icon")
-      )
+    ButtonGroup(disabled = true)(
+      Button(onClick = e => println("one"))("One"),
+      Button(onClick = e => println("two"))("Two"),
+      Button(onClick = e => println("three"))("Three")
     )
 
 }
